e0f45f6a7eac12a7e76dbffe18b0ae3aafd732f3
omikking <omikparmani333@gmail.com>
2026-06-27 00:10:23 +0530

    updated .gitignore

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"ticket-system/internal/auth"
+	"ticket-system/internal/config"
+	"ticket-system/internal/router"
+	"ticket-system/internal/store"
+)
+
+func main() {
+	
+	cfg := config.Load()
+
+	db := store.NewInMemoryStore()
+	jwtManager := auth.NewJWTManager(cfg.JWTSecret)
+
+	handler := router.New(db, jwtManager)
+
+	addr := ":" + cfg.Port
+	log.Printf("ticket-system listening on %s", addr)
+
+	if err := http.ListenAndServe(addr, handler); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
+}
